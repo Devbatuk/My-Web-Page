ef3501b154f4b339b012afc8822f716d70ac4eac
Batuhan Kurtul <batuhankurtul17@gmail.com>
2023-05-15 20:37:55 +0300

    second commit

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -7,7 +7,7 @@ export default function MainContent() {
                 <img src={MainFoto} alt="resim bulunamadı" />
             </div>
             <div className="mainContent-text">
-                Merhaba ben Batuhan. 16 yaşındayım, Nevzat Ayaz Anadolu Lisesinde 10.sınıf öğrencisiyim. Yazılım sektörüne ilk olarak
+                Merhaba, ben Batuhan. 16 yaşındayım, Nevzat Ayaz Anadolu Lisesinde 10.sınıf öğrencisiyim. Yazılım sektörüne ilk olarak
                 2020 yılında adım attım. Yazılıma C# öğrenerek başladım daha sonrasında Cpp, Java dillerinde kısa bir tecrübem oldu. Yaklaşık bir yıl önce
                 Html öğrenerek web tarafına ilk adımımı atmış oldum. Kendimi bu dilde geliştirdikten sonra Css ve Javascripte de gelişmeye ve yeni şeyler
                 öğrenip projeler geliştirmeye devam ettim, o günden bu yana da kendimi eğitmeye ve yeni şeyler öğrenmeye devam ediyorum.
